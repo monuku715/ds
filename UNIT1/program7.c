994db37923aa01f52c2a66b114365cfc8c4b4ba0
Monu developer <monukumarpatel715@gmail.com>
2026-08-26 21:20:41 +0530

    Add files via upload

---
+++ UNIT1/program7.c
@@ -0,0 +1,32 @@
+*/7. Delete an element from the array from user defined position. 
+NAME:-MONU KUMAR PATEL
+ENROLL:-92500527141*/
+
+#include <stdio.h>
+
+void main() {
+    int a[20], i, pos, n;
+
+    printf("\nEnter Limit: ");
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++) {
+        printf("\nEnter value %d: ", i + 1);
+        scanf("%d", &a[i]);
+    }
+    printf("\nEnter position to delete: ");
+    scanf("%d", &pos);
+
+    pos = pos - 1;
+
+    for(i = pos; i < n - 1; i++) {
+        a[i] = a[i + 1];
+    }
+
+    n--;
+
+    printf("\nArray after deletion:\n");
+    for(i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+}

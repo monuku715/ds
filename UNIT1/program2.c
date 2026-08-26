8bdc0211227ddf08ee794bb220606855f17b35a3
Monu developer <monukumarpatel715@gmail.com>
2026-08-26 21:32:40 +0530

    Add files via upload

---
+++ UNIT1/program2.c
@@ -0,0 +1,31 @@
+*/create an array of size 10,input values and display sum and average of all element in the array.
+NAME:-MONU KUMAR PATEL
+ENROLL:-92500527141*/
+#include <stdio.h>
+
+int main(){
+
+     int arr[10];
+     int sum =0;
+     float average;
+
+     for(int i=0;i<10;i++){
+        printf("enter your array element at index %d:",i+1);
+        scanf(" %d",&arr[i]);
+        sum = sum + arr[i];
+
+     }
+
+     printf("your entered element are:");
+     for(int i=0;i<10;i++){
+      printf(" %d",arr[i]);
+     }
+
+     average=sum/10;
+
+     printf("\nsum of all the element of the array are: %d",sum);
+     printf("\nyour average of array element are: %.2f",average);
+       
+    
+     return 0;
+}
